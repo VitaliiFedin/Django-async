794dfc78bf2d1b12373dc9b2f0bd7310f2320ae2
VitaliiFedin <vitalii.fedinn@gmail.com>
2022-07-25 15:01:42 +0300

    compare synv vs async

--- hello_async/hello_async/urls.py
+++ hello_async/hello_async/urls.py
@@ -20,4 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.index),
+    path("async/", views.async_view),
+    path("sync/", views.sync_view),
 ]
